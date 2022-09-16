de296d1736358231b4a7742c38b82a673a1b9a05
Avery Kyle Arenas <kyruken@gmail.com>
2022-09-16 14:05:49 -0700

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ function App() {
       <div className="tenzies-container">
         {tenzies && <Confetti />}
         <h1>Tenzies</h1>
-        <p>Roll until all dice are the same. Click each die to freeze
+        <p className="instruction-text">Roll until all dice are the same. Click each die to freeze
           it at its current value between rolls.
         </p>
         <div className="dice-layout">{diceElements}</div>
