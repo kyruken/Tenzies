e66426b8d61f5f423d36fde125af36af74b00954
Avery Kyle Arenas <kyruken@gmail.com>
2022-09-16 12:14:23 -0700

    add nanoid and made dice object

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,38 @@
 import React from 'react'
 import Die from './assets/Die'
 import './App.css'
+import {nanoid} from 'nanoid'
 
 function App() {
-  const [dice, setDice] = React.useState([])
+  const [dice, setDice] = React.useState(createDice())
+
+  function createDice() {
+    const newDice = [{}]
+    for (let x = 0; x < 10; x++) {
+     newDice[x] = {
+      value: Math.floor(Math.random() * 6) + 1,
+      isHeld: false,
+      key: nanoid()
+    };
+    }
+    return newDice;
+  }
+
+  function holdDie(dieId) {
+    setDice(prevDice => prevDice.map(die => {
+      return die.id === dieId ?  die.isHeld = !die.isHeld : die
+    }))
+  }
+
+  console.log(dice)
+  const diceElements = dice.map(die => {
+    return <Die value={die.value} key={die.key} holdDie={holdDie} id={die.key} isHeld={die.isHeld}/>
+  })
+
   return (
     <div>
       <div className="tenzies">
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
+        {diceElements}
       </div>
 
     </div>
--- src/assets/Die.jsx
+++ src/assets/Die.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Die(props) {
     return (
-        <div className="die">
+        <div className="die" onClick={(event) => props.holdDie(props.id)}>
             {props.value}
         </div>
     )
