5ce3f5d4e18e1edaee91e4f27b0c9481344d41f7
Avery Kyle Arenas <kyruken@gmail.com>
2022-09-16 11:35:51 -0700

    lay out dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import Die from './assets/Die'
+import './App.css'
 
 function App() {
-
+  const [dice, setDice] = React.useState([])
   return (
     <div>
+      <div className="tenzies">
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+      </div>
 
     </div>
   )
